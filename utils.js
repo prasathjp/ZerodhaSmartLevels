cddf4321782aabad78a58703dae208631eac7375
prasathjp <prasath_dhan@yahoo.co.in>
2026-07-28 14:51:19 +0530

    Refactor target calculations and add highlighting logic
    Refactor target calculation functions and add shouldHighlight function.

--- utils.js
+++ utils.js
@@ -31,21 +31,49 @@ window.ZSL = (() => {
         );
     }
 
-    function nextTarget(price, interval = 100) {
+    function shouldHighlight(price) {
 
-        return Math.ceil(price / interval) * interval;
+    let target = 0;
+    let threshold = 0;
+
+    if (price < 10) {
+
+        target = 10;
+        threshold = 0.5;
+
+    } else if (price < 100) {
+
+        target = Math.ceil(price / 10) * 10;
+        threshold = target * 0.03;      // 30→0.9, 50→1.5, 90→2.7
+
+    } else if (price < 1000) {
+
+        target = Math.ceil(price / 100) * 100;
+        threshold = target * 0.02;      // 100→2, 200→4 ... 900→18
+
+    } else if (price < 10000) {
+
+        target = Math.ceil(price / 1000) * 1000;
+        threshold = target * 0.01;      // 1000→10 ... 9000→90
+
+    } else {
+
+        target = Math.ceil(price / 10000) * 10000;
+        threshold = target * 0.01;      // 20000→200, 30000→300...
 
     }
+
+    return {
+        target,
+        distance: +(target - price).toFixed(2),
+        highlight: price >= (target - threshold)
+    };
+
+}
 
     function previousTarget(price, interval = 100) {
 
         return Math.floor(price / interval) * interval;
-
-    }
-
-    function distance(price, target) {
-
-        return +(target - price).toFixed(2);
 
     }
 
@@ -55,24 +83,6 @@ window.ZSL = (() => {
             return 0;
 
         return +(((target - price) / price) * 100).toFixed(2);
-
-    }
-
-    function getHighlightClass(diff) {
-
-        if (diff <= 5)
-            return "zsl-green";
-
-        if (diff <= 10)
-            return "zsl-yellow";
-
-        if (diff <= 20)
-            return "zsl-orange";
-
-        if (diff <= 50)
-            return "zsl-red";
-
-        return "";
 
     }
 
